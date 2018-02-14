32959e263cd76025b79def029c4e75bd9137ec87
Nass <mezouar.nass@gmail.com>
2018-02-14 15:27:38 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:create, :index, :show, :new, :edit, ] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
